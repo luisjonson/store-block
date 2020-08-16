16ad522a6976525653222f2e0202cff6e6f16f8b
luisjonson <luizlindonjonson@yahoo.com.br>
2020-08-15 23:30:49 -0300

    "countdown"

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 interface CountdownProps { }
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+  return( <div>
+    <h1>Teste Countdown</h1>
+  </div>
+  )
 }
 
 Countdown.schema = {
@@ -13,4 +16,4 @@ Countdown.schema = {
   properties: {},
 }
 
-export default Countdown
+ Countdown
